c4642188ae39c9ba6aa9ed3ba25e43fd009a662a
Eigendev <dupontdev@gmail.com>
2021-06-30 14:19:26 -0400

    implement type traits

--- src/traits.h
+++ src/traits.h
@@ -9,30 +9,35 @@
 //                  PRIMTIIVE TYPE TRAITS
 //==========================================================================
 template <typename T>
-struct is_primitive {
+struct is_1D_primitive {
   static const bool value = false;
 };
 
 template<>
-struct is_primitive<hydro1d::Primitive>
+struct is_1D_primitive<hydro1d::Primitive>
 {
     static constexpr bool value = true;
 };
 
 template<>
-struct is_primitive<hydro2d::Primitive>
+struct is_1D_primitive<sr1d::Primitive>
+{
+    static constexpr bool value = true;
+};
+
+template<typename T>
+struct is_2D_primitive {
+    static const bool value = false;
+};
+
+template<>
+struct is_2D_primitive<hydro2d::Primitive>
 {
     static constexpr bool value = true;
 };
 
 template<>
-struct is_primitive<sr1d::Primitive>
-{
-    static constexpr bool value = true;
-};
-
-template<>
-struct is_primitive<sr2d::Primitive>
+struct is_2D_primitive<sr2d::Primitive>
 {
     static constexpr bool value = true;
 };
