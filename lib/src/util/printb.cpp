178fac849cbce529673d1a38bd22184d9c39d828
Marcus DuPont <dupontdev@gmail.com>
2021-11-15 13:38:50 -0500

    queik typo

--- lib/src/util/printb.cpp
+++ lib/src/util/printb.cpp
@@ -1,16 +1,1 @@
-#include "printb.hpp"
 
-namespace simbi
-{
-    namespace util
-    {
-        void write (std::string const & str) {
-                std::cout << str;
-        }
-        
-        void writeln (std::string const & str) {
-                std::cout << str << '\n';
-        }
-    } // namespace util
-    
-} // namespace simbi
