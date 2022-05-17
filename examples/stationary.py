ab875398ff6b14c70c121a72741e70f32bd83ce9
Marcus DuPont <dupontdev@gmail.com>
2022-05-17 17:40:01 -0400

    couple added features

--- examples/stationary.py
+++ examples/stationary.py
@@ -20,11 +20,14 @@ def main():
     parser.add_argument('--bc', '-bc',    dest='boundc', type=str, default='outflow', choices=['outflow', 'inflow', 'reflecting', 'periodic'])
     parser.add_argument('--mode', '-m',   dest='mode', type=str, default='cpu', choices=['gpu', 'cpu'])    
     parser.add_argument('--data_dir', '-d',   dest='data_dir', type=str, default='data/') 
-    
+    parser.add_argument('--tex', dest='tex', action='store_true', default=False)
     args = parser.parse_args()
     
+    if args.tex:
+        plt.rc('text', usetex=True)
+        
     stationary = ((1.4, 1.0, 0.0), (1.0, 1.0, 0.0))
-    fig, ax = plt.subplots(1, 1, figsize=(10,10))
+    fig, ax = plt.subplots(1, 1, figsize=(3.5,2.78))
 
     hydro = Hydro(gamma=args.gamma, initial_state = stationary,
             Npts=args.nzones, geometry=(0.0,1.0,0.5), n_vars=3)
@@ -45,15 +48,16 @@ def main():
 
 
     x = np.linspace(0, 1, args.nzones)
-    fig.suptitle("Stationary Wave Problem at t = {} with N = {}".format(args.tend, args.nzones))
+    fig.suptitle("Stationary Wave Problem at $t$ = {} with $N =$ {}".format(args.tend, args.nzones))
     ax.plot(x, poll[0], 'r--', fillstyle='none', label='HLLE')
     ax.plot(x, bar [0], 'b', label='HLLC')
-    ax.set_xlabel('X', fontsize=20)
-    ax.set_ylabel('Density', fontsize=20)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel(r'$\rho / \rho_0$')
 
 
     ax.legend()
     ax.set_xlim(0, 1)
+    fig.savefig('stationary.pdf', bbox_inches='tight')
     plt.show()
 
 if __name__ == '__main__':
