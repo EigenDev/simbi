a3d8815fe651ec8bc7e6b3441811054ef404604c
Marcus DuPont <dupontdev@gmail.com>
2022-06-09 22:36:06 -0400

    thinking of universal argparse somehow

--- tools/movie.py
+++ tools/movie.py
@@ -223,15 +223,16 @@ def plot_polar_plot(fig, axs, cbaxes, fields, args, mesh, dset, subplots=False):
                 if cbar_orientation == 'vertical':
                     ycoord  = [0.1, 0.1]
                     xcoord  = [0.88, 0.04]
-                    cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.8]) for i in range(num_fields)]
+                    # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.8]) for i in range(num_fields)]
                 else:
                     ycoord  = [0.15, 0.15]
                     xcoord  = [0.51, 0.06]
-                    cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.43, 0.05]) for i in range(num_fields)]
+                    # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.43, 0.05]) for i in range(num_fields)]
             else:
                 cbar_orientation = 'horizontal'
                 if cbar_orientation == 'horizontal':
-                    cbaxes  = fig.add_axes([0.15, 0.15, 0.70, 0.05]) 
+                    pass
+                    # cbaxes  = fig.add_axes([0.15, 0.15, 0.70, 0.05]) 
         else:  
             if not args.no_cbar:         
                 cbar_orientation = args.cbar_orient
@@ -241,7 +242,7 @@ def plot_polar_plot(fig, axs, cbaxes, fields, args, mesh, dset, subplots=False):
                         if cbar_orientation == 'vertical':
                             ycoord  = [0.1, 0.08] if x2max < np.pi else [0.15, 0.15]
                             xcoord  = [0.1, 0.85] if x2max < np.pi else [0.93, 0.08]
-                            cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.65]) for i in range(num_fields)]
+                            # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.65]) for i in range(num_fields)]
                         else:
                             if is_wedge:
                                 ycoord  = [0.2, 0.20] if x2max < np.pi else [0.15, 0.15]
@@ -249,25 +250,25 @@ def plot_polar_plot(fig, axs, cbaxes, fields, args, mesh, dset, subplots=False):
                             else:
                                 ycoord  = [0.2, 0.20] if x2max < np.pi else [0.10, 0.10]
                                 xcoord  = [0.1, 0.50] if x2max < np.pi else [0.51, 0.20]
-                            cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.45, 0.04]) for i in range(num_fields)]
+                            # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.45, 0.04]) for i in range(num_fields)]
                     if num_fields == 3:
                         ycoord  = [0.1, 0.5, 0.1]
                         xcoord  = [0.07, 0.85, 0.85]
-                        cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.4]) for i in range(1, num_fields)]
-                        cbaxes.append(fig.add_axes([xcoord[0], ycoord[0] ,0.03, 0.8]))
+                        # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.4]) for i in range(1, num_fields)]
+                        # cbaxes.append(fig.add_axes([xcoord[0], ycoord[0] ,0.03, 0.8]))
                     if num_fields == 4:
                         ycoord  = [0.5, 0.1, 0.5, 0.1]
                         xcoord  = [0.85, 0.85, 0.07, 0.07]
-                        cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.8/(0.5 * num_fields)]) for i in range(num_fields)]
+                        # cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.8/(0.5 * num_fields)]) for i in range(num_fields)]
                 else:
                     if not is_wedge:
                         pass
                         #plt.tight_layout()
                         
-                    if cbar_orientation == 'vertical':
-                        cbaxes  = fig.add_axes([0.86, 0.07, 0.03, 0.85])
-                    else:
-                        cbaxes  = fig.add_axes([0.86, 0.07, 0.03, 0.90])
+                    # if cbar_orientation == 'vertical':
+                    #     # cbaxes  = fig.add_axes([0.86, 0.07, 0.03, 0.85])
+                    # else:
+                    #     # cbaxes  = fig.add_axes([0.86, 0.07, 0.03, 0.90])
         if args.log:
             if not args.no_cbar:
                 if num_fields > 1:
@@ -316,8 +317,8 @@ def plot_polar_plot(fig, axs, cbaxes, fields, args, mesh, dset, subplots=False):
     else:
         ax.axes.yaxis.set_ticklabels([])
         
-    ax.axes.xaxis.set_ticklabels([])
-    ax.axes.yaxis.set_ticklabels([])
+    # ax.axes.xaxis.set_ticklabels([])
+    # ax.axes.yaxis.set_ticklabels([])
     ax.set_rmax(x1max) if args.rmax == 0.0 else ax.set_rmax(args.rmax)
     ax.set_rmin(x1min)
     
@@ -504,55 +505,22 @@ def main():
         description='Plot a 2D Figure From a File (H5).',
         epilog="This Only Supports H5 Files Right Now")
     
-    parser.add_argument('data_dir', metavar='dir', nargs='+',
-                        help='A data directory to retrieve the h5 files')
-    
-    parser.add_argument('setup', metavar='Setup', nargs='+', type=str,
-                        help='The name of the setup you are plotting (e.g., Blandford McKee)')
-    
-    parser.add_argument('--field', dest = "field", metavar='Field Variable', nargs='+',
-                        help='The name of the field variable you\'d like to plot',
-                        choices=field_choices, default=["rho"])
-    parser.add_argument('--rmax', dest = "rmax", metavar='Radial Domain Max',
-                        default = 0.0, help='The domain range')
-    
-    parser.add_argument('--cbar_range', dest = "cbar", metavar='Range of Color Bar(s)', nargs='+',
-                        default = [None, None], help='The colorbar range you\'d like to plot')
-    
-    parser.add_argument('--cbar_sub', dest = "cbar2", metavar='Range of Color Bar for secondary plot',nargs='+',type=float,
-                        default =[None, None], help='The colorbar range you\'d like to plot')
-    
-    parser.add_argument('--cmap', dest = "cmap", metavar='Color Bar Colarmap',
-                        default = 'magma', help='The colorbar cmap you\'d like to plot')
-    parser.add_argument('--cmap2', dest = "cmap2", metavar='Color Bar #2 Colarmap',
-                        default = 'magma', help='The colorbar cmap you\'d like to plot')
-    parser.add_argument('--log', dest='log', action='store_true',
-                        default=False,
-                        help='Logarithmic Radial Grid Option')
-    
-    parser.add_argument('--first_order', dest='forder', action='store_true',
-                        default=False,
-                        help='True if this is a grid using RK1')
-    
-    parser.add_argument('--rev_cmap', dest='rcmap', action='store_true',
-                        default=False,
-                        help='True if you want the colormap to be reversed')
-    
-    parser.add_argument('--x', dest='x', nargs="+", default = None, type=float,
-                        help='List of x values to plot field max against')
-    
-    parser.add_argument('--xlabel', dest='xlabel', nargs=1, default = 'X',
-                        help='X label name')
-    
-    parser.add_argument('--tex', dest='tex', action='store_true',
-                        default=False, help='True if you want the latex formatting')
-    
-    parser.add_argument('--labels', dest='labels', nargs="+", default = None,
-                        help='Optionally give a list of labels for multi-file plotting')
-    
-    parser.add_argument('--tidx', dest='tidx', type=int, default = None,
-                        help='Set to a value if you wish to plot a 1D curve about some angle')
-    
+    parser.add_argument('data_dir', metavar='dir', nargs='+',help='A data directory to retrieve the h5 files')
+    parser.add_argument('setup', metavar='Setup', nargs='+', type=str, help='The name of the setup you are plotting (e.g., Blandford McKee)')
+    parser.add_argument('--field', dest = "field", metavar='Field Variable', nargs='+',help='The name of the field variable you\'d like to plot',choices=field_choices, default=["rho"])
+    parser.add_argument('--rmax', dest = "rmax", metavar='Radial Domain Max',default = 0.0, help='The domain range')
+    parser.add_argument('--cbar_range', dest = "cbar", metavar='Range of Color Bar(s)', nargs='+',default = [None, None], help='The colorbar range you\'d like to plot')
+    parser.add_argument('--cbar_sub', dest = "cbar2", metavar='Range of Color Bar for secondary plot',nargs='+',type=float,default =[None, None], help='The colorbar range you\'d like to plot')
+    parser.add_argument('--cmap', dest = "cmap", metavar='Color Bar Colarmap', default = 'magma', help='The colorbar cmap you\'d like to plot')
+    parser.add_argument('--cmap2', dest = "cmap2", metavar='Color Bar #2 Colarmap', default = 'magma', help='The colorbar cmap you\'d like to plot')
+    parser.add_argument('--log', dest='log', action='store_true',default=False, help='Logarithmic Radial Grid Option')
+    parser.add_argument('--first_order', dest='forder', action='store_true',default=False, help='True if this is a grid using RK1')
+    parser.add_argument('--rev_cmap', dest='rcmap', action='store_true', default=False, help='True if you want the colormap to be reversed')
+    parser.add_argument('--x', dest='x', nargs="+", default = None, type=float,help='List of x values to plot field max against')
+    parser.add_argument('--xlabel', dest='xlabel', nargs=1, default = 'X', help='X label name')
+    parser.add_argument('--tex', dest='tex', action='store_true',default=False, help='True if you want the latex formatting')
+    parser.add_argument('--labels', dest='labels', nargs="+", default = None,help='Optionally give a list of labels for multi-file plotting')
+    parser.add_argument('--tidx', dest='tidx', type=int, default = None, help='Set to a value if you wish to plot a 1D curve about some angle')
     parser.add_argument('--nwedge', dest='nwedge', default=0, type=int, help='Number of wedges')
     parser.add_argument('--wedge_lims', dest='wedge_lims', default = [0.4, 1.4, 80, 110], type=float, nargs=4)
     parser.add_argument('--units', dest='units', default = False, action='store_true')
@@ -562,14 +530,21 @@ def main():
     parser.add_argument('--bipolar', dest='bipolar', default = False, action='store_true')
     parser.add_argument('--pictorial', dest='pictorial', default = False, action='store_true')
     parser.add_argument('--fig_dims', dest='fig_dims', default = [3.35, 9], type=float, nargs=2)
-    parser.add_argument('--save', dest='save', type=str,
-                        default=None,
-                        help='Save the fig with some name')
-    
-    parser.add_argument('--half', dest='half', action='store_true',
-                        default=False,
-                        help='True if you want half a polar plot')
-        
+    parser.add_argument('--save', dest='save', type=str, default=None, help='Save the fig with some name')
+    parser.add_argument('--half', dest='half', action='store_true', default=False, help='True if you want half a polar plot')
+    parser.add_argument('--no_cbar', dest='no_cbar', help="Set if ignore cbar", action='store_true', default=False)
+    parser.add_argument('--cbar_orient', dest='cbar_orient', default='vertical', type=str, help='Colorbar orientation', choices=['horizontal', 'vertical'])
+    parser.add_argument('--xlims', dest='xlims', default = None, type=float, nargs=2)
+    parser.add_argument('--ylims', dest='ylims', default = None, type=float, nargs=2)
+    parser.add_argument('--print', dest='print', default = False, action='store_true')
+    parser.add_argument('--subplots', dest='subplots', default = None, type=int)
+    parser.add_argument('--sub_split', dest='sub_split', default = None, nargs='+', type=int)
+    parser.add_argument('--anot_loc', dest='anot_loc', default = None, type=str)
+    parser.add_argument('--legend_loc', dest='legend_loc', default = None, type=str)
+    parser.add_argument('--anot_text', dest='anot_text', default = None, type=str)
+    parser.add_argument('--inset', dest='inset', action= 'store_true', default=False)
+    parser.add_argument('--png', dest='png', action= 'store_true', default=False)
+    parser.add_argument('--tau_s', dest='tau_s', action= 'store_true', default=False, help='The shock optical depth')
     args = parser.parse_args()
     
     if args.tex:
@@ -604,7 +579,7 @@ def main():
             pass
         else:
             args.cbar += (num_fields - 1) * [None, None]
-            
+    
     # read the first file and infer the system configuration from it
     init_setup = util.read_2d_file(args, args.data_dir[0] + flist[0])[1]
     if init_setup["is_cartesian"]:
@@ -647,9 +622,8 @@ def main():
                     if args.pictorial:
                         cbaxes  = [fig.add_axes([xcoord[i], ycoord[i] ,0.03, 0.8/(0.5 * num_fields)]) for i in range(num_fields)]
             else:
-                if args.pictorial:
-                    pass
-                    # cbaxes  = fig.add_axes([0.8, 0.1, 0.03, 0.8]) 
+                if not args.pictorial:
+                    cbaxes  = fig.add_axes([0.8, 0.1, 0.03, 0.8]) 
             cbar_orientation = "vertical"
     
     def init_mesh(filename):
