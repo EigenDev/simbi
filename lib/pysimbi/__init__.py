3885ffb1fbdd39abf9d22c60736d8ab5a35d4896
Marcus DuPont <dupontdev@gmail.com>
2022-09-26 18:35:46 -0400

    version

--- lib/pysimbi/__init__.py
+++ lib/pysimbi/__init__.py
@@ -1,1 +1,2 @@
 from .simbi import Hydro
+from .version import __version__, __short_version__
---
+++ lib/pysimbi/version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Version info"""
+
+__short_version__ = '0.8'
+__version__ = '0.8.0'
