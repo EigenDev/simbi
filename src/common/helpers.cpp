73265152f83abc66ac339bdbfb467562bd964d77
Marcus DuPont <dupontdev@gmail.com>
2022-10-03 20:06:03 -0400

    no need for try block

--- src/common/helpers.cpp
+++ src/common/helpers.cpp
@@ -24,21 +24,21 @@ namespace simbi
             return "Keyboard interrupt detected. Saving last checkpoint...";
         }
 
-        // void catch_signals() {
-        //     // Adapted from answer to "How can I catch a ctrl-c event? (C++)"
-        //     struct sigaction sigBreakHandler;
-        //     sigBreakHandler.sa_handler = [](int stat) {throw InterruptException(stat) ;};
-        //     sigemptyset(&sigBreakHandler.sa_mask);
-        //     sigBreakHandler.sa_flags = 0;
-        //     sigaction(SIGINT, &sigBreakHandler, NULL);
-        // }
-
         void catch_signals() {
-            auto handler = [](int code) { throw std::runtime_error("SIGNAL " + std::to_string(code)); };
-            signal(SIGINT, handler);
-            signal(SIGTERM, handler);
-            signal(SIGKILL, handler);
+            // Adapted from answer to "How can I catch a ctrl-c event? (C++)"
+            struct sigaction sigBreakHandler;
+            sigBreakHandler.sa_handler = [](int stat) {throw InterruptException(stat) ;};
+            sigemptyset(&sigBreakHandler.sa_mask);
+            sigBreakHandler.sa_flags = 0;
+            sigaction(SIGINT, &sigBreakHandler, NULL);
         }
+
+        // void catch_signals() {
+        //     auto handler = [](int code) { throw std::runtime_error("SIGNAL " + std::to_string(code)); };
+        //     signal(SIGINT, handler);
+        //     signal(SIGTERM, handler);
+        //     signal(SIGKILL, handler);
+        // }
         // =========================================================================================================
         //        HELPER FUNCTIONS FOR COMPUTATION
         // =========================================================================================================
