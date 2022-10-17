bae1e146dca1e3f554bf1b5064596060deab2775
Marcus DuPont <dupontdev@gmail.com>
2022-10-16 22:36:52 -0400

    working entry point with example setup script

--- pysimbi/__init__.py
+++ pysimbi/__init__.py
@@ -1,3 +1,8 @@
 from .simbi import Hydro
-from .version import __version__
+from .config import BaseConfig 
 from .helpers import print_problem_params
+from pathlib import Path
+
+script_path = Path(__file__, '..').resolve()
+with open(script_path.joinpath('VERSION')) as vfile:
+    __version__ = vfile.readline()
