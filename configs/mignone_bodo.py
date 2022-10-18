37ea81cd5670fb7cde0316c18318ce32aeb7cf05
Marcus DuPont <dupontdev@gmail.com>
2022-10-17 22:22:27 -0400

    add Mignone and Bodo test problems 1 and 2

---
+++ configs/mignone_bodo.py
@@ -0,0 +1,38 @@
+from pysimbi import BaseConfig, DynamicArg
+class MignoneBodo(BaseConfig):
+    """
+    Mignone & Bodo (2005), Relativistic Test Problems in 1D Fluid
+    """
+    nzones    = DynamicArg("nzones", 1000, help="number of grid zones", var_type=int)
+    ad_gamma  = DynamicArg("gamma", 4.0 / 3.0, help="Adiabatic gas index", var_type = float)
+    problem   = DynamicArg("problem", 1, help = "test problem to compute", var_type=int, choices=[1,2])
+    @property
+    def initial_state(self):
+        if self.problem == 1:
+            return ((1.0, 10.0, -0.6), (10.0, 20.0, 0.5))
+        else:
+            return ((1.0, 1.0, 0.9), (1.0, 10.0, 0.5))
+    
+    @property
+    def geometry(self):
+        return (0.0, 1.0, 0.5)
+
+    @property
+    def linspace(self):
+        return True
+    
+    @property
+    def coord_system(self):
+        return "cartesian"
+
+    @property
+    def dimensions(self):
+        return self.nzones.default 
+    
+    @property
+    def gamma(self):
+        return self.ad_gamma.default 
+    
+    @property
+    def regime(self):
+        return "relativistic"
