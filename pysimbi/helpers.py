d886af675bcfaafe0a21513f8e9c0aaac89f9892
Marcus DuPont <dupontdev@gmail.com>
2022-11-07 23:50:35 -0500

    variable volume calculation

--- pysimbi/helpers.py
+++ pysimbi/helpers.py
@@ -8,7 +8,10 @@ def calc_cell_volume1D(r: np.ndarray) -> np.ndarray:
     return rmean * rmean * dr 
 
 def calc_cell_volume2D(r: np.ndarray, theta: np.ndarray) -> np.ndarray:
-    rr, thetta = np.meshgrid(r, theta)
+    if r.ndim == 1 and theta.ndim == 1:
+        rr, thetta = np.meshgrid(r, theta)
+    else:
+        rr, thetta = r, theta 
     tvertices = 0.5 * (thetta[1:] + thetta[:-1])
     tvertices = np.insert(tvertices, 0, thetta[0], axis=0)
     tvertices = np.insert(tvertices, tvertices.shape[0], thetta[-1], axis=0)
@@ -17,7 +20,6 @@ def calc_cell_volume2D(r: np.ndarray, theta: np.ndarray) -> np.ndarray:
     rvertices = np.sqrt(rr[:, 1:] * rr[:, :-1])
     rvertices = np.insert(rvertices,  0, rr[:, 0], axis=1)
     rvertices = np.insert(rvertices, rvertices.shape[1], rr[:, -1], axis=1)
-    dr        = rvertices[:, 1:] - rvertices[:, :-1]
     return (2.0 * np.pi *  (1./3.) * (rvertices[:, 1:]**3 - rvertices[:, :-1]**3) *  dcos)
 
 def print_problem_params(args, parser) -> None:
