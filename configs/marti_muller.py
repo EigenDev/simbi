2ac34a3c5dfa2ab1230a12954ba33b2a1060d588
Marcus DuPont <dupontdev@gmail.com>
2022-12-03 16:22:04 -0500

    pressure seems too low for MI50

--- configs/marti_muller.py
+++ configs/marti_muller.py
@@ -8,7 +8,7 @@ class MartiMuller(BaseConfig):
     ad_gamma  = DynamicArg("ad_gamma", 4.0 / 3.0, help="Adiabatic gas index", var_type = float)
     @property
     def initial_state(self):
-        return ((10.0, 0.0, 13.33), (1.0, 0.0, 0.0))
+        return ((10.0, 0.0, 13.33), (1.0, 0.0, 1e-10))
     
     @property
     def geometry(self):
@@ -34,22 +34,24 @@ class MartiMuller(BaseConfig):
     def regime(self):
         return "relativistic"
     
-    @property
-    def scale_factor(self):
-        return lambda t: 1 
+    #-------------------- Uncomment if one wants the mesh to move
     
-    @property
-    def scale_factor_derivative(self):
-        return lambda t: 0.5
+    # @property
+    # def scale_factor(self):
+    #     return lambda t: 1 
     
-    @property
-    def dens_outer(self):
-        return lambda x: 0.1 
+    # @property
+    # def scale_factor_derivative(self):
+    #     return lambda t: 0.5
     
-    @property
-    def mom_outer(self):
-        return lambda x: 0
+    # @property
+    # def dens_outer(self):
+    #     return lambda x: 0.1 
     
-    @property
-    def edens_outer(self):
-        return lambda x: 3e-10
+    # @property
+    # def mom_outer(self):
+    #     return lambda x: 0
+    
+    # @property
+    # def edens_outer(self):
+    #     return lambda x: 3e-10
