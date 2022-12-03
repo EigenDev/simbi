9b4b1e531bb272b4567ef03fcdeff345da510e28
Marcus DuPont <dupontdev@gmail.com>
2022-12-03 16:34:19 -0500

    phi in wrong place

--- pysimbi/helpers.py
+++ pysimbi/helpers.py
@@ -20,7 +20,7 @@ def calc_cell_volume2D(r: np.ndarray, theta: np.ndarray) -> np.ndarray:
     rvertices = np.sqrt(rr[:, 1:] * rr[:, :-1])
     rvertices = np.insert(rvertices,  0, rr[:, 0], axis=1)
     rvertices = np.insert(rvertices, rvertices.shape[1], rr[:, -1], axis=1)
-    return (1./3.) * (rvertices[:, 1:]**3 - rvertices[:, :-1]**3) *  dcos * dphi
+    return 2.0 * np.pi * (1./3.) * (rvertices[:, 1:]**3 - rvertices[:, :-1]**3) *  dcos
 
 def calc_cell_volume3D(r: np.ndarray, theta: np.ndarray, phi: np.ndarray) -> np.ndarray:
     if r.ndim == 1 and theta.ndim == 1 and phi.ndim == 1:
@@ -41,7 +41,7 @@ def calc_cell_volume3D(r: np.ndarray, theta: np.ndarray, phi: np.ndarray) -> np.
     rvertices = np.sqrt(rr[:, :,  1:] * rr[:, :, :-1])
     rvertices = np.insert(rvertices,  0, rr[:, :, 0], axis=2)
     rvertices = np.insert(rvertices, rvertices.shape[2], rr[:, :, -1], axis=2)
-    return (2.0 * np.pi *  (1./3.) * (rvertices[:, :, 1:]**3 - rvertices[:, :, :-1]**3) *  dcos)
+    return ((1./3.) * (rvertices[:, :, 1:]**3 - rvertices[:, :, :-1]**3) * dcos * dphi)
 
 def print_problem_params(args, parser) -> None:
     print("\nProblem paramters:")
