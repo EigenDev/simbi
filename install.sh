b1b655666581292ed93deb6b82a447b8e4f14724
Marcus DuPont <dupontdev@gmail.com>
2022-12-05 04:05:06 -0500

    no need for bin

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ GPU_INCLUDE="${GPU_RUNTIME_DIR}/include"
 HDF5_HEADER="$(echo '#include <H5Cpp.h>' | cpp -H -o /dev/null 2>&1 | head -n1 | sed "s/^.//;s/^.//")"
 if [ $? -ne 0 ]
 then
-HDF5_RUNTIME_DIR="$(echo $PATH | sed "s/:/\n/g" | grep "hdf5/bin" | sed "s/\/bin//g" |  head -n 1)"
+HDF5_RUNTIME_DIR="$(echo $PATH | sed "s/:/\n/g" | grep "hdf5" | sed "s/\/bin//g" |  head -n 1)"
 HDF5_INCLUDE="${HDF5_RUNTIME_DIR}/include"
 else
 HDF5_INCLUDE="$(dirname "${HDF5_HEADER}")"
