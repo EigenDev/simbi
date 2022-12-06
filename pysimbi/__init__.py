285036b9890153895436b4849cd4d7608178c560
Marcus DuPont <dupontdev@gmail.com>
2022-12-06 16:24:09 -0500

    follow git version control pattern

--- pysimbi/__init__.py
+++ pysimbi/__init__.py
@@ -1,9 +1,9 @@
 from .simbi import Hydro
 from .config import * 
 from .dynarg import DynamicArg
+from .version import __version_tuple__ 
 from .helpers import print_problem_params, compute_num_polar_zones, calc_dlogt
 from pathlib import Path
 
-script_path = Path(__file__, '..').resolve()
-with open(script_path.joinpath('VERSION')) as vfile:
-    __version__ = vfile.readline()
+
+__version__ = '.'.join(map(str,__version_tuple__[:3]))
