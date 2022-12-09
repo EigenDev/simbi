aff11592f9f0c70917723a2c21da61f8c901f22a
Marcus DuPont <dupontdev@gmail.com>
2022-12-08 21:35:40 -0500

    dont' ask to cleanup anymore just do

--- cleanup.sh
+++ cleanup.sh
@@ -4,23 +4,9 @@ SIMBI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 BUILD_DIR="${SIMBI_DIR}/build"
 EGG_DIR="${SIMBI_DIR}/pysimbi.egg-info"
 if [ -d "${EGG_DIR}" ]; then
-  while true; do
-    read -p "Do you wish to delete the ${EGG_DIR} directory (it is safe to do so)? " yn
-    case $yn in
-        [Yy]* ) rm -rf ${EGG_DIR}; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+  rm -rf ${EGG_DIR}
 fi
 
 if [ -d "${BUILD_DIR}" ]; then
-  while true; do
-    read -p "Do you wish to delete the ${BUILD_DIR} directory (it is safe to do so)? " yn
-    case $yn in
-        [Yy]* ) rm -rf ${BUILD_DIR}; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+  rm -rf ${BUILD_DIR}
 fi
