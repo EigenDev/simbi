445fc29353e613780f774bd962d2b18ac9563eb8
Marcus DuPont <dupontdev@gmail.com>
2022-12-19 18:48:04 -0500

    short verbose

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ function usage {
     echo "  --twod                         block size for 2D simulations (only set for gpu compilation)"
     echo "  --threed                       block size for 3D simulations (only set for gpu compilation)"
     echo "  --dev-arch                     SM architecture specification for gpu compilation"
-    echo "  --verbose                      flag for verbose compilation output"
+    echo "  -v | --verbose                 flag for verbose compilation output"
     echo "  --configure                    flag to only configure the meson build directory without installing"
     echo "  --[float | double]-precision   floating point precision"
     echo "  --[column | row]-major         memory layout for multi-dimensional arrays"
