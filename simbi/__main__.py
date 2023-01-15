cdb23801ebc7e7392f5f17c417d07d4780380cac
Marcus DuPont <dupontdev@gmail.com>
2023-01-15 04:21:20 -0500

    fix multi field plotting mishap

--- simbi/__main__.py
+++ simbi/__main__.py
@@ -9,6 +9,11 @@ from simbi import Hydro
 from pathlib import Path
 from .key_types import Optional, Sequence 
 
+derived = ['D', 'momentum', 'energy', 'energy_rst', 'enthalpy', 'temperature', 'T_eV', 'mass', 'chi_dens',
+          'mach', 'u1', 'u2']
+field_choices = ['rho', 'v1', 'v2', 'v3', 'v', 'p', 'gamma_beta', 'chi'] + derived
+lin_fields    = ['chi', 'gamma_beta', 'u1', 'u2', 'u3']
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -45,6 +50,7 @@ def get_subparser(parser: argparse.ArgumentParser, idx: int) -> argparse.Argumen
         for _, subparser in action.choices.items()
     ]
     return subparser[idx]
+
 def parse_run_arguments(parser: argparse.ArgumentParser):
     run_parser = get_subparser(parser, 0)
     overridable = run_parser.add_argument_group('override', 'overridable simuations options')
@@ -71,7 +77,46 @@ def parse_run_arguments(parser: argparse.ArgumentParser):
     global_args.add_argument('--type-check', help='flag for static type checking configration files', default=True, action=argparse.BooleanOptionalAction)
     return parser, parser.parse_known_args(args=None if sys.argv[2:] else ['run', '--help'])
 
-    
+
+def parse_plotting_arguments(parser: argparse.ArgumentParser) -> argparse.Namespace:
+    plot_parser = get_subparser(parser, 1)
+    plot_parser.add_argument('files',        nargs='+', help='A Data Source to Be Plotted')
+    plot_parser.add_argument('setup',        type=str, help='The name of the setup you are plotting (e.g., Blandford McKee)')
+    plot_parser.add_argument('--fields',     default=['rho'], nargs='+', help='The name of the field variable you\'d like to plot',choices=field_choices)
+    plot_parser.add_argument('--xmax',       default = 0.0, help='The domain range')
+    plot_parser.add_argument('--log',        default=False, action='store_true',  help='Logarithmic Radial Grid Option')
+    plot_parser.add_argument('--kinetic',    default=False, action='store_true',  help='Plot the kinetic energy on the histogram')
+    plot_parser.add_argument('--enthalpy',   default=False, action='store_true',  help='Plot the enthalpy on the histogram')
+    plot_parser.add_argument('--hist',       default=False, action='store_true',  help='Convert plot to histogram')
+    plot_parser.add_argument('--mass',       default=False, action='store_true',  help='Compute mass histogram')
+    plot_parser.add_argument('--dm_du',      default = False, action='store_true', help='Compute dM/dU over whole domain')
+    plot_parser.add_argument('--ax_anchor',  default=None, type=str, nargs='+', help='Anchor annotation text for each plot')
+    plot_parser.add_argument('--norm',       default=False, action='store_true', help='True if you want the plot normalized to max value')
+    plot_parser.add_argument('--labels',     default = None, nargs='+', help='Optionally give a list of labels for multi-file plotting')
+    plot_parser.add_argument('--xlims',      default = None, type=float, nargs=2)
+    plot_parser.add_argument('--ylims',      default = None, type=float, nargs=2)
+    plot_parser.add_argument('--units',      default = False, action='store_true')
+    plot_parser.add_argument('--power',      default = 1.0, type=float, help='exponent of power-law norm')
+    plot_parser.add_argument('--dbg',        default = False, action='store_true')
+    plot_parser.add_argument('--tex',        default = False, action='store_true')
+    plot_parser.add_argument('--print',      default = False, action='store_true')
+    plot_parser.add_argument('--pictorial',  default = False, action='store_true')
+    plot_parser.add_argument('--anot_loc',   default = None, type=str)
+    plot_parser.add_argument('--legend_loc', default = None, type=str)
+    plot_parser.add_argument('--anot_text',  default = None, type=str)
+    plot_parser.add_argument('--inset',      default=False, action= 'store_true')
+    plot_parser.add_argument('--png',        default=False, action= 'store_true')
+    plot_parser.add_argument('--fig_dims',   default = [4, 4], type=float, nargs=2)
+    plot_parser.add_argument('--legend',     default=True, action=argparse.BooleanOptionalAction)
+    plot_parser.add_argument('--extra_files',default=None, nargs='+', help='extra 1D files to plot alongside 2D plots')
+    plot_parser.add_argument('--save',       default=None, help='Save the fig with some name')
+    plot_parser.add_argument('--kind',       default='snapshot', type=str, choices=['snapsoht', 'movie'])
+    plot_parser.add_argument('--cmap',       default='viridis', type=str, help='matplotlib color map')
+    plot_parser.add_argument('--nplots',     default=1, type=int, help='number of subplots')
+    plot_parser.add_argument('--cbar_range', default = [None, None], dest = 'cbar', nargs=2, help='The colorbar range')
+    plot_parser.add_argument('--fill_scale', type=float, default = None, help='Set the y-scale to start plt.fill_between')
+    return parser, parser.parse_args(args=None if sys.argv[2:] else ['--help'])
+
 def parse_module_arguments():
     parser = CustomParser(prog='simbi', usage='%(prog)s <setup_script> [options]', description="Relativistic gas dynamics module")
     parser.add_argument('--version','-V', help='print current version of simbi module', action=print_the_version)
@@ -222,9 +267,10 @@ def run(parser: argparse.ArgumentParser, *_) -> None:
         print(state_docs[idx], flush=True)
         print("="*80, flush=True)
         sim_state.simulate(**kwargs[idx])
-
+            
 def plot_checkpoints(parser: argparse.ArgumentParser, args: argparse.Namespace, argv: list) -> None:
     from .plot import main 
+    parser, args = parse_plotting_arguments(parser)
     main(parser, args, argv)
     
 def main():
--- simbi/plot.py
+++ simbi/plot.py
@@ -11,46 +11,8 @@ field_choices = ['rho', 'v1', 'v2', 'v3', 'v', 'p', 'gamma_beta', 'chi'] + deriv
 lin_fields    = ['chi', 'gamma_beta', 'u1', 'u2', 'u3']
 
 tool_src = Path(__file__).resolve().parent / 'tools'
-def parse_extra_arguments(parser: argparse.ArgumentParser) -> argparse.Namespace:
-    parser.add_argument('files',  nargs='+', help='A Data Source to Be Plotted')
-    parser.add_argument('setup',  type=str, help='The name of the setup you are plotting (e.g., Blandford McKee)')
-    parser.add_argument('--fields',     default=['rho'], nargs='+', help='The name of the field variable you\'d like to plot',choices=field_choices)
-    parser.add_argument('--xmax',       default = 0.0, help='The domain range')
-    parser.add_argument('--log',        default=False, action='store_true',  help='Logarithmic Radial Grid Option')
-    parser.add_argument('--kinetic',    default=False, action='store_true',  help='Plot the kinetic energy on the histogram')
-    parser.add_argument('--enthalpy',   default=False, action='store_true',  help='Plot the enthalpy on the histogram')
-    parser.add_argument('--hist',       default=False, action='store_true',  help='Convert plot to histogram')
-    parser.add_argument('--mass',       default=False, action='store_true',  help='Compute mass histogram')
-    parser.add_argument('--dm_du',      default = False, action='store_true', help='Compute dM/dU over whole domain')
-    parser.add_argument('--ax_anchor',  default=None, type=str, nargs='+', help='Anchor annotation text for each plot')
-    parser.add_argument('--norm',       default=False, action='store_true', help='True if you want the plot normalized to max value')
-    parser.add_argument('--labels',     default = None, nargs='+', help='Optionally give a list of labels for multi-file plotting')
-    parser.add_argument('--xlims',      default = None, type=float, nargs=2)
-    parser.add_argument('--ylims',      default = None, type=float, nargs=2)
-    parser.add_argument('--units',      default = False, action='store_true')
-    parser.add_argument('--power',      default = 1.0, type=float, help='exponent of power-law norm')
-    parser.add_argument('--dbg',        default = False, action='store_true')
-    parser.add_argument('--tex',        default = False, action='store_true')
-    parser.add_argument('--print',      default = False, action='store_true')
-    parser.add_argument('--pictorial',  default = False, action='store_true')
-    parser.add_argument('--anot_loc',   default = None, type=str)
-    parser.add_argument('--legend_loc', default = None, type=str)
-    parser.add_argument('--anot_text',  default = None, type=str)
-    parser.add_argument('--inset',      default=False, action= 'store_true')
-    parser.add_argument('--png',        default=False, action= 'store_true')
-    parser.add_argument('--fig_dims',   default = [4, 4], type=float, nargs=2)
-    parser.add_argument('--legend',     default=True, action=argparse.BooleanOptionalAction)
-    parser.add_argument('--extra_files',default=None, nargs='+', help='extra 1D files to plot alongside 2D plots')
-    parser.add_argument('--save',       default=None, help='Save the fig with some name')
-    parser.add_argument('--kind',       default='snapshot', type=str, choices=['snapsoht', 'movie'])
-    parser.add_argument('--cmap',       default='viridis', type=str, help='matplotlib color map')
-    parser.add_argument('--nplots',     default=1, type=int, help='number of subplots')
-    parser.add_argument('--cbar_range', default = [None, None], dest = 'cbar', nargs=2, help='The colorbar range')
-    parser.add_argument('--fill_scale', type=float, default = None, help='Set the y-scale to start plt.fill_between')
-    return parser, parser.parse_known_args(args=None if sys.argv[2:] else ['--help'])
 
-def main(parser: argparse.ArgumentParser = None, args: argparse.Namespace = None, argv: list[str] = None) -> None:
-    parser, (args, argv) = parse_extra_arguments(parser)
+def main(parser: argparse.ArgumentParser = None, args: argparse.Namespace = None, *_) -> None:
     if args.tex:
         plt.rc('font',   size=DEFAULT_SIZE)          # controls default text sizes
         plt.rc('axes',   titlesize=DEFAULT_SIZE)     # fontsize of the axes title
@@ -68,7 +30,7 @@ def main(parser: argparse.ArgumentParser = None, args: argparse.Namespace = None
                 "font.size": DEFAULT_SIZE
             }
         )
-        
+    
     sys.path.insert(1, f'{tool_src}')
     file_list, _  = get_file_list(args.files)
     ndim          = get_dimensionality(file_list)
