fix pad calculation mistake

simbi/initial_condition.py
@@ -146,5 +146,6 @@ def initializeModel(model: Any, first_order: bool, volume_factor: Union[float, N
     if passive_scalars is not None:
         model.u[-1,...] = passive_scalars
     
-    npad = ((0,0),) + tuple(tuple(val) for val in [[2 * (first_order + 1), 2 * (first_order + 1)]] * model.dimensionality) 
+    
+    npad = ((0,0),) + tuple(tuple(val) for val in [[((first_order^1) + 1),  ((first_order^1) + 1)]] * model.dimensionality) 
     model.u = np.pad(model.u, npad, 'edge')  * volume_factor