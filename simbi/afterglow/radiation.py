83bbd6b1102b8df49064baa44457ecf56e6d3dae
Marcus DuPont <dupontdev@gmail.com>
2023-03-04 04:01:13 -0500

    compactify

--- simbi/afterglow/radiation.py
+++ simbi/afterglow/radiation.py
@@ -57,7 +57,7 @@ def parse_args(
         '--ntbins',
         type=int,
         help='number of time bins',
-        default=50)
+        default=128)
     afterglow_parser.add_argument(
         '--theta-samples',
         type=int,
@@ -75,9 +75,9 @@ def parse_args(
         nargs='+',
         default=None)
     afterglow_parser.add_argument(
-        '--extra-files',
+        '--load',
         type=str,
-        help='data file from self computed light curves',
+        help='data file from simbi-computed light curves',
         default=None,
         nargs='+')
     afterglow_parser.add_argument(
@@ -92,8 +92,7 @@ def parse_args(
         type=float,
         default=[0.25, 0.75])
     afterglow_parser.add_argument(
-        '--dfile_out',
-        dest='dfile_out',
+        '--output',
         help='name of file to be saved as',
         type=str,
         default='some_lc.h5')
@@ -101,7 +100,7 @@ def parse_args(
         '--example-labels',
         help='label(s) of the example curve\'s markers',
         type=str,
-        default=['example'],
+        default=[],
         nargs='+')
     afterglow_parser.add_argument(
         '--xlims',
@@ -181,9 +180,30 @@ def parse_args(
     afterglow_parser.add_argument(
         '--labels',
         help='list of label names for simbi checkpoints',
-        default=None,
+        default=[],
         type=str,
         nargs='+'
+    )
+    afterglow_parser.add_argument(
+        '--legend-loc',
+        help = 'location of legend',
+        default=None,
+        type=str,
+        choices = [
+            'lower left',
+            'lower right',
+            'lower center',
+            'center',
+            'upper right',
+            'upper left',
+            'upper center'
+        ]
+    )
+    afterglow_parser.add_argument(
+        '--vline',
+        help='location of vertical line in plot',
+        default=None,
+        type=float
     )
 
     return parser, parser.parse_args(
@@ -193,6 +213,7 @@ def parse_args(
 def run(parser: argparse.ArgumentParser = None,
         args: argparse.Namespace = None,
         *_):
+    
     parser, args = parse_args(parser, args)
 
     scales = Scale(args.scale)
@@ -204,7 +225,7 @@ def run(parser: argparse.ArgumentParser = None,
 
     files, _ = util.get_file_list(args.files)
     fig, ax  = plt.subplots(figsize=args.fig_dims)
-    freqs    = np.array(args.nu) * units.Hz
+    freqs    = np.asarray(args.nu) * units.Hz
 
     if args.cmap is not None:
         vmin, vmax = args.clims
@@ -213,21 +234,23 @@ def run(parser: argparse.ArgumentParser = None,
         colors = util.get_colors(cinterval, cmap, vmin, vmax)
     else:
         colors = ['c', 'y', 'm', 'k']  # list of basic colors
-
-    dim = util.get_dimensionality(files)
-    
+        
+    #---------------------------------------------------------
+    # Calculations
+    #---------------------------------------------------------
     if args.mode != 'checkpoint':
+        dim = util.get_dimensionality(files)
         nbins = args.ntbins
         nbin_edges = nbins + 1
-        tbin_edge = get_tbin_edges(args, files, scales.time_scale)
-        tbin_edges = np.geomspace(
-            tbin_edge[0] * 0.9,
-            tbin_edge[1] * 1.1,
-            nbin_edges)
-        time_bins = np.sqrt(tbin_edges[1:] * tbin_edges[:-1])
-        fnu = {i: np.zeros(nbins) * units.mJy for i in args.nu}
-        events_list = np.zeros(shape=(len(files), 2))
-        storage = {}
+        if args.mode == 'fnu':
+            tbin_edge = get_tbin_edges(args, files, scales.time_scale)
+            tbin_edges = np.geomspace(
+                tbin_edge[0] * 0.9,
+                tbin_edge[1] * 1.1,
+                nbin_edges)
+            time_bins = np.sqrt(tbin_edges[1:] * tbin_edges[:-1])
+            fnu = {i: np.zeros(nbins) * units.mJy for i in args.nu}
+            
         scales_dict = {
             'time_scale': scales.time_scale.value,
             'length_scale': scales.length_scale.value,
@@ -235,31 +258,30 @@ def run(parser: argparse.ArgumentParser = None,
             'pre_scale': scales.pre_scale.value,
             'v_scale': 1.0
         }
-        theta_obs = np.deg2rad(args.theta_obs)
-        dL = get_dL(args.z)
+        
         sim_info = {
-            'theta_obs': theta_obs,
-            'nus': freqs.value,
-            'z': args.z,
-            'd_L': dL.value,
+            'theta_obs': np.deg2rad(args.theta_obs),
+            'nus':   freqs.value,
+            'z':     args.z,
+            'd_L':   get_dL(args.z).value,
             'eps_e': args.eps_e,
             'eps_b': args.eps_b,
-            'p': args.p,
+            'p':     args.p,
         }
 
-        if args.mode == 'fnu':
-            for idx, file in enumerate(files):
-                fields, setup, mesh = util.read_file(args, file, dim)
-                # Generate a pseudo mesh if computing off-axis afterglows
-                generate_pseudo_mesh(
-                    args,
-                    mesh,
-                    full_sphere=True,
-                    full_threed=not args.theta_obs == 0)
-                sim_info['dt'] = setup['dt']
-                sim_info['adiabatic_gamma'] = setup['ad_gamma']
-                sim_info['current_time'] = setup['time']
-                t1 = pytime.time()
+        for idx, file in enumerate(files):
+            fields, setup, mesh = util.read_file(args, file, dim)
+            # Generate a pseudo mesh if computing off-axis afterglows
+            generate_pseudo_mesh(
+                args,
+                mesh,
+                full_sphere=True,
+                full_threed=not args.theta_obs == 0)
+            sim_info['dt'] = setup['dt']
+            sim_info['adiabatic_gamma'] = setup['ad_gamma']
+            sim_info['current_time'] = setup['time']
+            t1 = pytime.time()
+            if args.mode == 'fnu':
                 py_calc_fnu(
                     fields=fields,
                     tbin_edges=tbin_edges.value,
@@ -270,23 +292,7 @@ def run(parser: argparse.ArgumentParser = None,
                     chkpt_idx=idx,
                     data_dim=dim
                 )
-                print(
-                    f"Processed file {file} in {pytime.time() - t1:.2f} s",
-                    flush=True)
-        elif args.mode == 'events':
-            for idx, file in enumerate(files):
-                fields, setup, mesh = util.read_file(args, file, dim)
-                # Generate a pseudo mesh if computing off-axis afterglows
-                generate_pseudo_mesh(
-                    args,
-                    mesh,
-                    full_sphere=True,
-                    full_threed=not args.theta_obs == 0)
-                photon_distro = np.zeros(shape=(mesh['phi']))
-                sim_info['dt'] = setup['dt']
-                sim_info['adiabatic_gamma'] = setup['ad_gamma']
-                sim_info['current_time'] = setup['time']
-                t1 = pytime.time()
+            else:
                 py_log_events(
                     fields=fields,
                     photon_distro=photon_distro,
@@ -296,40 +302,40 @@ def run(parser: argparse.ArgumentParser = None,
                     sim_info=sim_info,
                     data_dim=dim,
                 )
-                print(
-                    f"Processed file {file} in {pytime.time() - t1:.2f} s",
-                    flush=True)
-                print(x_mu)
-                zzz = input('')
-
+            print(
+                f"Processed file {file} in {pytime.time() - t1:.2f} s",
+                flush=True)
+                
         for key in fnu.keys():
             fnu[key] *= (1 + args.z)
 
         # Save the data
-        if args.mode != 'checkpoint':
-            file_name = args.dfile_out
-            if os.path.splitext(file_name)[1] != '.h5':
-                file_name += '.h5'
+        file_name = args.output
+        if os.path.splitext(file_name)[1] != '.h5':
+            file_name += '.h5'
 
-            isFile = os.path.isfile(file_name)
-            dirname = os.path.dirname(file_name)
-            if os.path.exists(dirname) == False and dirname != '':
-                if not isFile:
-                    # Create a new directory because it does not exist
-                    os.makedirs(dirname)
-                    print(80 * '=')
-                    print(f"creating new directory named {dirname}...")
+        isFile  = os.path.isfile(file_name)
+        dirname = os.path.dirname(file_name)
+        if os.path.exists(dirname) == False and dirname != '':
+            if not isFile:
+                # Create a new directory because it does not exist
+                os.makedirs(dirname)
+                print(80 * '=')
+                print(f"creating new directory named {dirname}...")
 
-            print(80 * "=")
-            print(f"Saving file as {file_name}...")
-            print(80 * '=')
-            with h5py.File(file_name, 'w') as hf:
-                fnu_save = np.array([fnu[key] for key in fnu.keys()])
-                dset = hf.create_dataset('sogbo_data', dtype='f')
-                hf.create_dataset('nu', data=[nu for nu in args.nu])
-                hf.create_dataset('fnu', data=fnu_save)
-                hf.create_dataset('tbins', data=time_bins)
+        print(80 * "=")
+        print(f"Saving file as {file_name}...")
+        print(80 * '=')
+        with h5py.File(file_name, 'w') as hf:
+            fnu_save = np.array([fnu[key] for key in fnu.keys()])
+            # dset = hf.create_dataset('sogbo_data', dtype='f')
+            hf.create_dataset('nu', data=[nu for nu in args.nu])
+            hf.create_dataset('fnu', data=fnu_save)
+            hf.create_dataset('tbins', data=time_bins)
 
+    #--------------------------------------------------
+    # Plotting
+    #--------------------------------------------------
     lines = ["--", "-.", ":", "-"]
     linecycler = cycle(lines)
     color_cycle = cycle(colors)
@@ -344,16 +350,18 @@ def run(parser: argparse.ArgumentParser = None,
         if front_part == 1.0:
             label = f'{var_label}' + r'10^{%d}$' % (power_of_ten) + (' day' if args.spectra else ' Hz')
         else:
-            label = f'{var_label}' + r'%f \times 10^{%fid}$' % (front_part, power_of_ten) + (' day' if args.spectra else ' Hz')
+            label = f'{var_label}' + r'%d \times 10^{%d}$' % (int(front_part), power_of_ten) + (' day' if args.spectra else ' Hz')
         
-        if args.mode == 'compute':
+        if args.mode != 'checkpoint':
             if args.spectra:
                 nearest_dat    = util.find_nearest(time_bins.value, time)[0]
                 relevant_flux  = np.asanyarray([fnu[key][nearest_dat].value for key in fnu.keys()])
+                xarr           = args.nu
             else:
-                relevant_flux = fnu[var]
-                
-            ax.plot(relevant_var, relevent_flux, color=color, label=label)
+                relevant_flux = fnu[val]
+                xarr = time_bins
+
+            ax.plot(xarr, relevant_flux, color=color, label=label)
         
         if args.example_data:
             for dat in args.example_data:
@@ -362,15 +370,16 @@ def run(parser: argparse.ArgumentParser = None,
                     key = util.find_nearest(example_data['tday'].value, val)[1] * units.day
                     x = example_data['freq'] * units.Hz 
                     y = example_data['spectra'][key]
+                    m = 1.0
                 else:
                     key = val * units.Hz
                     x = example_data['tday']
                     y = example_data['fnu'][key]
-                    
-                ax.plot(x, y, 'o', color=color, markersize=0.5)
+                    m = 0.5
+                ax.plot(x, y, 'o', color=color, markersize=m)
 
-        if args.extra_files:
-            for dfile in args.extra_files:
+        if args.load:
+            for dfile in args.load:
                 linestyle = next(linecycler)
                 dat = read_simbi_afterglow(dfile)
                 if args.spectra:
@@ -380,7 +389,7 @@ def run(parser: argparse.ArgumentParser = None,
                 else:
                     x = dat['tday']
                     y = dat['fnu'][val * units.Hz]
-                    
+
                 ax.plot(x, y, color=color, label=label, linestyle=linestyle)
                 
         if label not in legend_labels:
@@ -390,7 +399,8 @@ def run(parser: argparse.ArgumentParser = None,
             legend_labels += [label]
         
         linecycler = cycle(lines)
-    if args.xlims is not None:
+        
+    if args.xlims:
         tbound1, tbound2 = np.asanyarray(args.xlims) * units.day
     else:
         tbound1 = time_bins[0] if 'time_bins' in locals() else dat['tday'][0]
@@ -405,7 +415,7 @@ def run(parser: argparse.ArgumentParser = None,
     ax.set_xscale('log')
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    ax.set_ylabel(r'$\rm Flux \ Density \ [\rm mJy]$')
+    ax.set_ylabel(r'Flux  Density [mJy]')
     if args.spectra:
         if args.xlims is not None:
             ax.set_xlim(*args.xlims)
@@ -414,25 +424,30 @@ def run(parser: argparse.ArgumentParser = None,
         ax.set_xlim(tbound1.value, tbound2.value)
         ax.set_xlabel(r'$t_{\rm obs} [\rm day]$')
 
+    if args.vline:
+        ax.axvline(args.vline, color='black', linestyle=':', alpha=0.8)
+        
     ex_lines = []
-    if args.example_data is not None:
-        marks = cycle(['o', 's'])
-        for label in args.example_labels:
-            ex_lines += [mlines.Line2D([0],
-                                       [0],
-                                       marker=next(marks),
-                                       color='w',
-                                       label=label,
-                                       markerfacecolor='grey',
-                                       markersize=5)]
-    if args.extra_files:
-        labels = [None] * len(args.extra_files)
-        if args.labels:
-            for label in args.labels:
-                ex_lines += [mlines.Line2D([0,1],[0,1], linestyle=next(linecycler), label=label, color='grey')]
+    marks = cycle(['o', 's'])
+    for label in args.example_labels:
+        ex_lines += [
+            mlines.Line2D([0],[0],
+            marker=next(marks),
+            color='w',
+            label=label,
+            markerfacecolor='grey',
+            markersize=5)
+        ]
+        
+    for label in args.labels:
+        ex_lines += [
+            mlines.Line2D([0,1],[0,1], 
+            linestyle=next(linecycler), 
+            label=label, 
+            color='grey')
+        ]
 
-    ax.legend(handles=[*sim_lines, *ex_lines])
-
+    ax.legend(handles=[*sim_lines, *ex_lines], loc=args.legend_loc)
     if args.save:
         file_str = f"{args.save}".replace(' ', '_')
         print(f'saving as {file_str}.pdf')
