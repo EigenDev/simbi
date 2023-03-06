45d781030d68460f0afbbef862aba8ff60d2bc95
Marcus DuPont <dupontdev@gmail.com>
2023-03-06 18:17:13 -0500

    getting closer to working thread pool

--- src/util/parallel_for.hpp
+++ src/util/parallel_for.hpp
@@ -4,91 +4,24 @@
 #include "util/launch.hpp"
 #include "util/range.hpp"
 #include "device_api.hpp"
-#include <omp.h>
-#include <thread>
-#include <condition_variable>
-#include <queue>
+#include "thread_pool.hpp"
 
 namespace simbi 
 {
-	namespace pooling {
-		/**
-		 * Practicing ThreadPooling based on this answer:
-		 * https://stackoverflow.com/a/32593825/13874039
-		*/
-		class ThreadPool {
-			public:
-				void Start() {
-					const unsigned num_threads = std::thread::hardware_concurrency();
-					threads.resize(num_threads);
-					for (unsigned i = 0; i < num_threads; i++) {
-						threads[i] = std::thread(&ThreadPool::ThreadLoop, this);
-					}
-				}
+	namespace {
+        inline unsigned int nthreads = ([] {
+            if(const char* thread_env = std::getenv("NTHREADS"))
+                return static_cast<unsigned int>(std::stoul(std::string(thread_env)));
 
-				void QueueJob(const std::function<void()>& job) {
-					{
-						std::unique_lock<std::mutex> lock(queue_mutex);
-						jobs.push(job);
-					}
-					mutex_condition.notify_one();
-				}
+            if(const char* thread_env = std::getenv("OMP_NUM_THREADS"))
+                return static_cast<unsigned int>(std::stoul(std::string(thread_env)));
 
-				void Stop() {
-					{
-						std::unique_lock<std::mutex> lock(queue_mutex);
-						should_terminate = true;
-					}
-					mutex_condition.notify_all();
-					for (std::thread& active_thread : threads) {
-						active_thread.join();
-					}
-					threads.clear();
-				}
+            return std::thread::hardware_concurrency();
+        })();
 
-				bool busy() {
-					bool poolbusy;
-					{
-						std::unique_lock<std::mutex> lock(queue_mutex);
-						poolbusy = jobs.empty();
-					}
-					return poolbusy;
-				}
-
-				~ThreadPool() {
-					Stop();
-				}
-
-				ThreadPool() {
-					Start();
-				}
-
-			private:
-				void ThreadLoop() {
-					while (true) {
-						std::function<void()> job;
-						{
-							std::unique_lock<std::mutex> lock(queue_mutex);
-							mutex_condition.wait(lock, [this] {
-								return !jobs.empty() || should_terminate;
-							});
-							if (should_terminate) {
-								return;
-							}
-							job = jobs.front();
-							jobs.pop();
-						}
-						job();
-					}
-				}
-
-				bool should_terminate = false;           // Tells threads to stop looking for jobs
-				std::mutex queue_mutex;                  // Prevents data races to the job queue
-				std::condition_variable mutex_condition; // Allows threads to wait on new jobs or termination 
-				std::vector<std::thread> threads;
-				std::queue<std::function<void()>> jobs;
-		};
-	}
+        inline auto thread_pool = simbi::pooling::ThreadPool(nthreads);
+    }
+	
 	template <typename index_type, typename F>
 	void parallel_for(index_type first, index_type last, F function)  {
 		ExecutionPolicy p(last - first);
@@ -101,12 +34,9 @@ namespace simbi
 			#if GPU_CODE
 				for (auto idx : range(first, last, globalThreadCount()))  function(idx);
 			#else	
-				// static auto thread_pool = pooling::ThreadPool();
-				// thread_pool.QueueJob([&] { 
-				// 	for(auto idx = first; idx < last; idx++) function(idx);
-				// });
-				#pragma omp parallel for schedule(static) 
-				for(auto idx = first; idx < last; idx++) function(idx);
+				thread_pool.parallel_for(first, last, function);
+				// #pragma omp parallel for schedule(static) 
+				// for(auto idx = first; idx < last; idx++) function(idx);
 			#endif
 			
 		});
---
+++ src/util/thread_pool.hpp
@@ -0,0 +1,111 @@
+#ifndef THREAD_POOL_HPP
+#define THREAD_POOL_HPP
+
+#include <thread>
+#include <condition_variable>
+#include <queue>
+#include <future>
+#include <iostream>
+
+namespace simbi {
+    namespace pooling {
+		/**
+		 * Practicing ThreadPooling based on this answer:
+		 * https://stackoverflow.com/a/32593825/13874039
+		*/
+		class ThreadPool {
+			public:
+				ThreadPool(const unsigned int nthreads) : nthreads(nthreads) {
+                    std::cout << "ctor called" << "\n";
+					threads.reserve(nthreads);
+					for (unsigned i = 0; i < nthreads; i++) {
+						threads.emplace_back(std::thread(&ThreadPool::ThreadLoop, this));
+					}
+				}
+
+				~ThreadPool() {
+					// Stop the thread pool and notify all threads ot finish the 
+					// remaining tasks
+					{
+						std::unique_lock<std::mutex> lock(queue_mutex);
+						should_terminate = true;
+					}
+					mutex_condition.notify_all();
+					for (std::thread& active_thread : threads) {
+						active_thread.join();
+					}
+					threads.clear();
+				}
+
+				void QueueJob(const std::function<void()>& job) {
+					{
+						std::unique_lock<std::mutex> lock(queue_mutex);
+						jobs.push(job);
+					}
+					mutex_condition.notify_one();
+				}
+
+				template<typename index_type, typename F>
+				void parallel_for(const index_type start, const index_type stop, const F func) {
+					static unsigned batch_size =  std::ceil((float)(stop - start) / (float)nthreads);
+					auto block_start = start - batch_size;
+					auto block_end   = start;
+					
+					auto step = [&] {
+						block_start += batch_size;
+						block_end   += batch_size;
+						block_end    = (block_end > stop) ? stop : block_end;
+					};
+
+					step();
+					for (auto &worker : threads)
+					{
+						QueueJob([=] {
+							for (auto q = block_start; q < block_end; q++) {
+								func(q);
+							}
+						});
+						step();
+					}
+				}
+
+				bool busy() {
+					bool poolbusy;
+					{
+						std::unique_lock<std::mutex> lock(queue_mutex);
+						poolbusy = jobs.empty();
+					}
+					return poolbusy;
+				}
+
+			private:
+				void ThreadLoop() {
+					while (true) {
+						std::function<void()> job;
+						// pop a job from queue and execute it
+						{
+							std::unique_lock<std::mutex> lock(queue_mutex);
+							mutex_condition.wait(lock, [this] {
+								return !jobs.empty() || should_terminate;
+							});
+							if (should_terminate) {
+								return;
+							}
+
+							job = std::move(jobs.front());
+							jobs.pop();
+						}
+						job();
+					}
+				}
+				unsigned int nthreads;
+				bool should_terminate = false;           // Tells threads to stop looking for jobs
+				std::mutex queue_mutex;                  // Prevents data races to the job queue
+				std::condition_variable mutex_condition; // Allows threads to wait on new jobs or termination 
+				std::vector<std::thread> threads;
+				std::queue<std::function<void()>> jobs;
+		};
+	}// namespace pooling
+} // namespace simbi
+
+#endif 
