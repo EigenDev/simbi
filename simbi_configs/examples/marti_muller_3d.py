e4bc88ea89bf951eca96da4efc5ee9ed1ba76ea3
Marcus DuPont <dupontdev@gmail.com>
2023-03-08 19:10:35 -0500

    fix type hint

--- simbi_configs/examples/marti_muller_3d.py
+++ simbi_configs/examples/marti_muller_3d.py
@@ -1,4 +1,4 @@
-from simbi import BaseConfig, DynamicArg, simbi_property, simbi_classproperty
+from simbi import BaseConfig, DynamicArg, simbi_property
 from simbi.key_types import *
 
 class MartiMuller3D(BaseConfig):
@@ -13,7 +13,7 @@ class MartiMuller3D(BaseConfig):
         return ((10.0, 0.0, 0.0, 0.0, 13.33), (1.0, 0.0, 0.0, 0.0, 1e-10))
     
     @simbi_property
-    def geometry(self) -> Sequence[float]:
+    def geometry(self) -> Sequence[Sequence[float]]:
         return ((0.0, 1.0, 0.5),
                 (0.0, 1.0),
                 (0.0,1.0))
@@ -27,7 +27,7 @@ class MartiMuller3D(BaseConfig):
         return "cartesian"
 
     @simbi_property
-    def resolution(self) -> DynamicArg:
+    def resolution(self) -> Sequence[DynamicArg]:
         return (self.nzones, self.nzones, self.nzones) 
     
     @simbi_property
