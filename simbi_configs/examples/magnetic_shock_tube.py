f3fde5f10d1982259639a4a92835d51a87afd3ea
Marcus DuPont <dupontdev@gmail.com>
2023-11-29 14:32:44 -0500

    add all 1d problems

--- simbi_configs/examples/magnetic_shock_tube.py
+++ simbi_configs/examples/magnetic_shock_tube.py
@@ -6,11 +6,19 @@ class MagneticShockTube(BaseConfig):
     """
     nzones    = DynamicArg("nzones", 100, help="number of grid zones", var_type=int)
     ad_gamma  = DynamicArg("ad-gamma", 2, help="Adiabatic gas index", var_type = float)
+    problem   = DynamicArg("problem", 1, help='problem number from Mignone & Bodo (2006)', var_type=int, choices=[1,2,3,4])
     
     @simbi_property
     def initial_state(self) -> Sequence[Sequence[float]]:
         # defined as (rho, v1, v2, v3, pg, b1, b2, b3)
-        return ((1.0, 0.0, 0.0, 0.0, 1.0, 0.5, 1.0, 0.0), (0.125, 0.0, 0.0, 0.0, 0.1, 0.5, -1.0, 0.0))
+        if self.problem == 1:
+            return ((1.0, 0.0, 0.0, 0.0, 1.0, 0.5, 1.0, 0.0), (0.125, 0.0, 0.0, 0.0, 0.1, 0.5, -1.0, 0.0))
+        elif self.problem == 2:
+            return ((1.0, 0.0, 0.0, 0.0, 30.0, 5.0, 6.0, 6.0), (1.0, 0.0, 0.0, 0.0, 1.0, 5.0, 0.7, 0.7))
+        elif self.problem == 3:
+            return ((1.0, 0.0, 0.0, 0.0, 1e3, 10.0, 7.0, 7.0), (1.0, 0.0, 0.0, 0.0, 0.1, 10.0, 0.7, 0.7))
+        else:
+            return ((1.0, 0.999, 0.0, 0.0, 0.1, 10.0, 7.0, 7.0), (1.0, -0.999, 0.0, 0.0, 0.1, 10.0, -7.0, -7.0))
     
     @simbi_property
     def geometry(self) -> Sequence[float]:
