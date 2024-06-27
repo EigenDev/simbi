fc31091f20caff9c8e476daf7761b70bcf1e92be
Marcus DuPont <md4469@nyu.edu>
2024-06-27 12:54:22 +0900

    formatting

--- simbi/detail/key_types.py
+++ simbi/detail/key_types.py
@@ -1,17 +1,52 @@
 from numpy.typing import NDArray
-from typing import Optional, Union, Sequence, Callable, final, Tuple, Any, cast, Iterable, Type
+from typing import (
+    Optional,
+    Union,
+    Sequence,
+    Callable,
+    final,
+    Tuple,
+    Any,
+    cast,
+    Iterable,
+    Type,
+)
 from numpy import float64 as numpy_float, string_ as numpy_string, int64 as numpy_int
 
 FloatOrArray = Union[float, NDArray[Any]]
-FloatOrNone  = Optional[float] 
-IntOrNone    = Optional[int] 
-ListOrNone   = Optional[list] 
-ArrayOrNone  = Optional[NDArray[Any]] 
-StrOrNone    = Optional[str] 
-BoolOrNone   = Optional[bool]
+FloatOrNone = Optional[float]
+IntOrNone = Optional[int]
+ListOrNone = Optional[list]
+ArrayOrNone = Optional[NDArray[Any]]
+StrOrNone = Optional[str]
+BoolOrNone = Optional[bool]
 SequenceOrNone = Optional[Sequence[Any]]
-CallableOrNone = Optional[Callable[...,Any]]
+CallableOrNone = Optional[Callable[..., Any]]
+ListOrArray = list[Any] | NDArray[Any]
 
-__all__ = ['Optional', 'Union', 'Sequence', 'Callable', 'final', 'Tuple', 'Any', 'NDArray', 'FloatOrArray',
-           'FloatOrNone', 'IntOrNone', 'ListOrNone', 'ArrayOrNone', 'StrOrNone', 'Type', 
-           'BoolOrNone', 'SequenceOrNone', 'CallableOrNone', 'cast', 'Iterable', 'numpy_float', 'numpy_int', 'numpy_string']
+__all__ = [
+    "Optional",
+    "Union",
+    "Sequence",
+    "Callable",
+    "final",
+    "Tuple",
+    "Any",
+    "NDArray",
+    "FloatOrArray",
+    "FloatOrNone",
+    "IntOrNone",
+    "ListOrNone",
+    "ArrayOrNone",
+    "StrOrNone",
+    "ListOrArray",
+    "Type",
+    "BoolOrNone",
+    "SequenceOrNone",
+    "CallableOrNone",
+    "cast",
+    "Iterable",
+    "numpy_float",
+    "numpy_int",
+    "numpy_string",
+]
