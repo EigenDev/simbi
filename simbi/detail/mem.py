34ac7b77e6f1527d1f7930a4073e3e430020ad83
Marcus DuPont <md4469@nyu.edu>
2024-10-26 12:03:57 -0400

    if a child process detects an exception, pass it to the main thread

--- simbi/detail/mem.py
+++ simbi/detail/mem.py
@@ -1,9 +1,25 @@
 from threading import Thread
 from typing import Any, Callable
+import queue
 
 def release_memory(func: Callable[...,Any]) -> Callable[...,Any]:
     def wrapper(*args: Any, **kwargs: Any) -> None:
-        p = Thread(target=func, args=args, kwargs=kwargs)
+        q: queue.Queue[None | Exception] = queue.Queue()
+
+        def thread_func(*args: Any, **kwargs: Any) -> None:
+            try:
+                func(*args, **kwargs)
+                q.put(None)  # No error
+            except Exception as e:
+                q.put(e)  # Put the exception in the queue
+
+        p = Thread(target=thread_func, args=args, kwargs=kwargs)
         p.start()
         p.join()
+
+        # Check if there was an exception
+        exception = q.get()
+        if exception is not None:
+            raise exception
+
     return wrapper
