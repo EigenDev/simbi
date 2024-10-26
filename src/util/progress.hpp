fabd971f3af7a5b6ce8f507b26f3481ad3746163
Marcus DuPont <md4469@nyu.edu>
2024-10-26 12:48:29 -0400

    portable progressbar to term edge

--- src/util/progress.hpp
+++ src/util/progress.hpp
@@ -19,6 +19,7 @@
 #define PROGRESS_HPP
 
 #include <iostream>
+#include <termios.h>
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
@@ -36,7 +37,7 @@ namespace simbi {
             CONSOLE_SCREEN_BUFFER_INFO csbi;
             GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
             const int columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-            return columns
+            return columns;
 #elif defined(__linux__) || defined(__APPLE__)
             struct winsize w;
             ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -46,8 +47,33 @@ namespace simbi {
 
         inline void progress_bar(double percentage)
         {
-            static int barWidth = get_term_width() / 5;
+            static int termWidth = get_term_width();
+            int cursorX          = 0;
+
+#if defined(_WIN32)
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+            cursorX = csbi.dwCursorPosition.X;
+#elif defined(__linux__) || defined(__APPLE__)
+            struct termios term, oldTerm;
+            tcgetattr(STDIN_FILENO, &term);
+            oldTerm = term;
+            term.c_lflag &= ~ICANON;
+            term.c_lflag &= ~ECHO;
+            tcsetattr(STDIN_FILENO, TCSANOW, &term);
+
+            std::cout << "\x1B[6n";
+            int cursorY;
+            scanf("\x1B[%d;%dR", &cursorY, &cursorX);
+
+            tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm);
+#endif
+
+            int availableWidth = termWidth - cursorX;
+            std::cout << "\033[K";   // Clear from cursor to end of line
             std::cout << "[";
+            int barWidth = availableWidth -
+                           10;   // Adjust for percentage display and brackets
             int pos = barWidth * percentage;
             for (int i = 0; i < barWidth; ++i) {
                 if (i < pos) {
