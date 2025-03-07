84069487980d3d785b081abe21531d72cf56a9d5
Marcus DuPont <md4469@nyu.edu>
2025-03-07 11:09:34 -0500

    some clean up

--- simbi/clone.py
+++ simbi/clone.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 
 setup_clone = """# Auto-generate setup script template to quickly get up
@@ -9,29 +8,24 @@ setup_clone = """# Auto-generate setup script template to quickly get up
 from simbi import (
     BaseConfig, 
     DynamicArg, 
-    simbi_classproperty, 
+    simbi_class_property, 
     simbi_property
 )
+from simbi.typing import InitialStateType
+from typing import Union, Sequence, Optional, Callable
 
 class {setup_name}(BaseConfig):
     \""" Some Hydro Problem
     A more descriptive doc string of what you're solving
     \"""
-    x = DynamicArg('x', value=1.0, help='a dummy dynamic arg', var_type=float)
+    class config:
+        x = DynamicArg('x', value=1.0, help='a dummy dynamic arg', var_type=float)
 
     def __init__(self) -> None:
-        \"""
-        initiate the problem here
-        \"""
         pass
 
     @simbi_property
-    def initial_state(self) -> Union[
-                                Sequence[
-                                    Union[NDArray[np.floating[Any]], 
-                                    Sequence[float]]], 
-                                    NDArray[np.floating[Any]]
-                               ]:
+    def initial_state(self) -> InitialStateType:
         raise NotImplementedError()
 
     @simbi_property
@@ -43,18 +37,11 @@ class {setup_name}(BaseConfig):
         raise NotImplementedError()
 
     @simbi_property
-    def resolution(self) -> Union[int,
-                                  Sequence[Union[int,
-                                                 DynamicArg]],
-                                  NDArray[numpy_int],
-                                  DynamicArg,
-                                  Sequence[Sequence[Union[int,
-                                                          DynamicArg]]]]:
+    def resolution(self) -> Union[Sequence[int], int]:
         raise NotImplementedError()
 
     @simbi_property
-    def bounds(self) -> Union[Sequence[Union[float, DynamicArg]],
-                                Sequence[Sequence[Union[float, DynamicArg]]]]:
+    def bounds(self) -> Union[Sequence[float], Sequence[Sequence[float]]]:
         raise NotImplementedError()
 
     @simbi_property
@@ -65,21 +52,11 @@ class {setup_name}(BaseConfig):
     def x1_spacing(self) -> str:
         return "linear"
 
-    @simbi_property
-    def sources(
-            self) -> Optional[Union[Sequence[NDArray[np.floating[Any]]], NDArray[np.floating[Any]]]]:
-        return None
-
-    @simbi_property
-    def passive_scalars(
-            self) -> Optional[Union[Sequence[float], NDArray[np.floating[Any]]]]:
-        return None
-
-    @simbi_classproperty
+    @simbi_class_property
     def scale_factor(cls) -> Optional[Callable[[float], float]]:
         return None
 
-    @simbi_classproperty
+    @simbi_class_property
     def scale_factor_derivative(cls) -> Optional[Callable[[float], float]]:
         return None
 
@@ -96,8 +73,7 @@ class {setup_name}(BaseConfig):
         return 'hllc'
 
     @simbi_property
-    def boundary_conditions(
-            self) -> Union[Sequence[str], str, NDArray[numpy_string]]:
+    def boundary_conditions(self) -> Sequence[str]:
         return 'outflow'
 
     @simbi_property
@@ -109,29 +85,9 @@ class {setup_name}(BaseConfig):
         return 'data/'
 
     @simbi_property
-    def dlogt(self) -> float:
-        return 0.0
-
-    @simbi_property
     def use_quirk_smoothing(self) -> bool:
         return False
-
-    @simbi_property
-    def x1(self) -> Optional[NDArray[np.floating[Any]]]:
-        return None
-
-    @simbi_property
-    def x2(self) -> Optional[NDArray[np.floating[Any]]]:
-        return None
-
-    @simbi_property
-    def x3(self) -> Optional[NDArray[np.floating[Any]]]:
-        return None
-
-    @simbi_property
-    def object_zones(self) -> Optional[Union[NDArray[Any], Sequence[Any]]]:
-        return None
-
+        
     @simbi_property
     def cfl_number(self) -> float:
         return 0.1
@@ -143,10 +99,6 @@ class {setup_name}(BaseConfig):
     @simbi_property
     def checkpoint_interval(self) -> float:
         return 0.1
-
-    @simbi_property
-    def engine_duration(self) -> float:
-        return 0.0
 """
 
 
