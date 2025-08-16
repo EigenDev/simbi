fd1c7c6059d605b8d63548c68f8c1f49ccb1ab6c
Marcus DuPont <md4469@nyu.edu>
2025-08-16 16:51:24 -0400

    name change

--- src/update/adaptive_timestep.hpp
+++ src/update/adaptive_timestep.hpp
@@ -66,7 +66,7 @@ namespace simbi {
     }
 
     template <typename HydroState, typename MeshConfig>
-    auto compute_timestep_async(const HydroState& state, const MeshConfig& mesh)
+    auto compute_timestep(const HydroState& state, const MeshConfig& mesh)
     {
         // create lazy timestep field
         auto timestep_field = create_timestep_field(state, mesh);
@@ -85,7 +85,7 @@ namespace simbi {
     template <typename HydroState, typename MeshConfig>
     void update_timestep(HydroState& state, const MeshConfig& mesh)
     {
-        auto dt_future    = compute_timestep_async(state, mesh);
+        auto dt_future    = compute_timestep(state, mesh);
         state.metadata.dt = dt_future.wait();
     }
 
