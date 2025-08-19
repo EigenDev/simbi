539d459dd027fe75804542305f354b62b3e7b503
Marcus DuPont <marcus.dupont@princeton.edu>
2025-08-19 11:57:13 -0400

    incl utility funcs

--- simbi/functional/__init__.py
+++ simbi/functional/__init__.py
@@ -5,7 +5,19 @@ from .helpers import (
     get_memory_usage,
 )
 from .maybe import Maybe
-from .utilities import compose, pipe
+from .utilities import (
+    compose,
+    pipe,
+    for_each,
+    map_with_index,
+    partition,
+    reduce_with_index,
+    curry,
+    group_by,
+    zip_with,
+    update_with,
+    memoize,
+)
 from .reader import LazySimulationReader, read_file
 
 __all__ = [
@@ -18,4 +30,13 @@ __all__ = [
     "LazySimulationReader",
     "read_file",
     "get_memory_usage",
+    "for_each",
+    "map_with_index",
+    "partition",
+    "reduce_with_index",
+    "curry",
+    "zip_with",
+    "group_by",
+    "update_with",
+    "memoize",
 ]
