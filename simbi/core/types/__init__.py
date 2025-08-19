e17b938fb4ad6b81d8408485bff9a34e7782b4b5
Marcus DuPont <marcus.dupont@princeton.edu>
2025-08-19 11:57:25 -0400

    new types

--- simbi/core/types/__init__.py
+++ simbi/core/types/__init__.py
@@ -4,9 +4,63 @@ Core type definitions for simbi.
 This module provides type definitions and constants used throughout the framework.
 """
 
+from .input import (
+    Solver,
+    CellSpacing,
+    TimeStepping,
+    Reconstruction,
+    ProcessedData,
+    Metadata,
+    FieldData,
+    RawHDF5,
+    MeshConfig,
+    Array,
+    IArray,
+    UArray,
+)
+
+from .bodies import (
+    BodyCapability,
+    Body,
+    GravitationalBody,
+    AccretionBody,
+    RigidBody,
+    DeformableBody,
+    ElasticBody,
+    BodyDiagnostics,
+    BodyData,
+    BaseBody,
+)
+
 from .typing import (
     InitialStateType,
     GasStateGenerator,
 )
 
-__all__ = ["InitialStateType", "GasStateGenerator"]
+__all__ = [
+    "InitialStateType",
+    "GasStateGenerator",
+    "Solver",
+    "CellSpacing",
+    "TimeStepping",
+    "Reconstruction",
+    "ProcessedData",
+    "Metadata",
+    "FieldData",
+    "BodyData",
+    "RawHDF5",
+    "MeshConfig",
+    "Array",
+    "BodyCapability",
+    "Body",
+    "GravitationalBody",
+    "AccretionBody",
+    "RigidBody",
+    "DeformableBody",
+    "ElasticBody",
+    "BodyData",
+    "BodyDiagnostics",
+    "BaseBody",
+    "IArray",
+    "UArray",
+]
