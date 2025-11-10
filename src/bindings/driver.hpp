89dcad7d4c46880b30bcd13d02807db26ff78e0e
Marcus DuPont <marcus.dupont@princeton.edu>
2025-11-10 12:47:43 -0500

    cleaning

--- src/bindings/driver.hpp
+++ src/bindings/driver.hpp
@@ -1,8 +1,6 @@
 #ifndef DRIVER_HPP
 #define DRIVER_HPP
 
-#include "compat.hpp"
-
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
@@ -12,16 +10,6 @@
 namespace py = pybind11;
 
 namespace simbi::driver {
-    // main driver function that takes NumPy array directly
-    // void run_simulation(
-    //     py::array_t<real, py::array::c_style> cons_array,
-    //     py::array_t<real, py::array::c_style> prim_array,
-    //     py::list staggered_bfields,
-    //     py::dict sim_info,
-    //     py::function a_func,
-    //     py::function adot_func
-    // );
-
     void run_simulation(
         py::iterator prim_gen,
         py::list staggered_bfields,
